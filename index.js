7426ead7151dcf9f814fef1949c4d1d4c5f1e5b4
Brandon <desselle.brandon2@gmail.com>
2019-05-24 07:44:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-require('dotenv').config()
-
-const server = require('./server.js')
-
-
+const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
-server.listen(port, () =>
-    console.log(`\n** API running on http://localhost:${port} **\n`)
-)
+server.listen(port, () => {
+    console.log(`server is running on localhost:${port}`)
+})
